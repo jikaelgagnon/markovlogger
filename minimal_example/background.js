6e1563533192d8fc85ebd15e21d55c91faf5c07c
jikael <jikaelgagnon@gmail.com>
2025-01-17 16:45:47 -0500

    Clean code and add timestamps to data

--- minimal_example/background.js
+++ minimal_example/background.js
@@ -1,34 +1,47 @@
 let tabs = new Map();
+let matches = chrome.runtime.getManifest().content_scripts[0].matches;
+console.log(matches[0]);
 
-add_new_tab = (tabId, url) => tabs.set(tabId, [url])
+function addEntry (tab) {
+  if (!tabs.has(tab.id)){
+    tabs.set(tab.id, new Map());
+    let m = tabs.get(tab.id);
+    m.set("url", [])
+    m.set("time", [])
+  }
+  tabs.get(tab.id).get("url").push(tab.url);
+  tabs.get(tab.id).get("time").push(Date.now());
+};
 
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
+  console.log(activeInfo);
   if (tabs.has(activeInfo.tabId)){return;}
 
   chrome.tabs.get(activeInfo.tabId, (tab) => {
       if (tab.url) { // Check if the tab has a URL
           console.log("Tab activated with URL:", tab.url);
-          add_new_tab(tab.id, tab.url);
-          console.log(tabs);
-
+          addEntry(tab);
       }
   });
 });
 
 chrome.tabs.onCreated.addListener((tab) => {
   console.log("tab created with url", tab.url);
-  add_new_tab(tab.id, tab)
-  console.log(tabs);
+  addEntry(tab);
 })
 
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    console.log(tab);
-    if (changeInfo.url) { // Check if the URL was updated
-        console.log("Tab URL updated:", changeInfo.url);
-        tabs.get(tabId).push(changeInfo.url);
-        console.log(tabs);
-    }
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url) { // Check if the URL was updated
+      console.log(changeInfo);
+      console.log("Tab URL updated:", changeInfo.url);
+      addEntry(tab);
+      console.log(tabs);
+      // console.log(tab);
+  }
 });
+
+// TODO: Add database/sheets
+// TODO: Add unique user ID
 
 
