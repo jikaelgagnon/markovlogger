b67754f32603d2064c4f4e01b581aa0c447606ef
jikael <jikaelgagnon@gmail.com>
2025-01-17 15:56:19 -0500

    Add URL tracking

--- minimal_example/background.js
+++ minimal_example/background.js
@@ -1,13 +1,34 @@
-let buttons = [];
+let tabs = new Map();
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.state)
-        buttons.push(request.state);
-        console.log(buttons);
-        sendResponse({received: "goodbye"});
+add_new_tab = (tabId, url) => tabs.set(tabId, [url])
+
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  if (tabs.has(activeInfo.tabId)){return;}
+
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+      if (tab.url) { // Check if the tab has a URL
+          console.log("Tab activated with URL:", tab.url);
+          add_new_tab(tab.id, tab.url);
+          console.log(tabs);
+
+      }
+  });
+});
+
+chrome.tabs.onCreated.addListener((tab) => {
+  console.log("tab created with url", tab.url);
+  add_new_tab(tab.id, tab)
+  console.log(tabs);
+})
+
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    console.log(tab);
+    if (changeInfo.url) { // Check if the URL was updated
+        console.log("Tab URL updated:", changeInfo.url);
+        tabs.get(tabId).push(changeInfo.url);
+        console.log(tabs);
     }
-  );
+});
+
+
